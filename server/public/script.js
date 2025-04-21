d5689b4c2e9969c602671e1f93a9e1b72fdfdb47
GaryDev <112358055+garydev10@users.noreply.github.com>
2025-04-21 22:30:39 +0100

    add schedules setup web ui

--- server/public/script.js
+++ server/public/script.js
@@ -57,8 +57,104 @@ function addEventBoiler() {
     });
 }
 
+function addEventSchedules(i, formId, dropdownId, jsonResultId) {
+    const form = document.getElementById(formId);
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        try {
+            const dropdown = document.getElementById(dropdownId);
+            const schedule = dropdown.value;
+            const res = await fetch('/api/schedules', {
+                method: 'post',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                },
+                body: JSON.stringify({
+                    schedule: schedule,
+                    index: i,
+                })
+            });
+            if (!res.ok) {
+                throw new Error(`Response status: ${res.status}`);
+            }
+            const data = await res.json();
+            const jsonResult = document.getElementById(jsonResultId);
+            jsonResult.innerText = JSON.stringify(data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    });
+}
+
+async function loadSchedules() {
+    let dropdownIds = [];
+    try {
+        let res = await fetch('/api/schedules');
+        let data = await res.json();
+        const schedules = data.schedules;
+        if (!schedules) return;
+
+        res = await fetch('/api/schedules-options');
+        if (!res.ok) {
+            throw new Error(`Response status: ${res.status}`);
+        }
+        data = await res.json();
+        const schedulesOptions = data.schedulesOptions;
+        if (!schedulesOptions) return;
+
+        let uiItems = schedules.map((schedule, i) => {
+            const di = i; // display id
+            const formId = `schedule${di}Form`;
+            const dropdownId = `schedule${di}`;
+            const jsonResultId = `schedule${di}JsonResult`;
+            return { di, formId, dropdownId, jsonResultId };
+        });
+
+        // init form
+        const formsHtml = schedules.map((schedule, i) => {
+            const { di, formId, dropdownId, jsonResultId } = uiItems[i];
+            const formHtml = `
+                <form id="${formId}" class="row">
+                    <label for="${dropdownId}" class="form-label">Schedule ${di} Boiler switch at: </label>
+                    <div class="col-sm-4">
+                        <select name="${dropdownId}" id="${dropdownId}" class="form-select">
+                            ${schedulesOptions.map((opt) => `
+                                <option value="${opt}" ${(opt === schedule) ? "selected" : ""}>
+                                    ${opt}
+                                </option>
+                            `).join('\n')}
+                        </select>
+                    </div>
+                    <div class="col-sm-4">
+                        <button type="submit" class="btn btn-primary">Schedule Set!</button>
+                    </div>
+                    <code id="${jsonResultId}"></code>
+                </form>
+                <hr />
+            `;
+            return formHtml;
+        }).join('\n');
+        const schedulesForms = document.getElementById("schedulesForms");
+        schedulesForms.innerHTML = formsHtml;
+
+        schedules.forEach((schedule, i) => {
+            const { dropdownId } = uiItems[i];
+            const dropdown = document.getElementById(dropdownId);
+            dropdown.selectedIndex = schedulesOptions.indexOf(schedule);
+        });
+
+        uiItems.forEach((uiItem, i) => {
+            const { formId, dropdownId, jsonResultId } = uiItem;
+            addEventSchedules(i, formId, dropdownId, jsonResultId);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     // main action after document load
     addEventWebCam();
     addEventBoiler();
+    await loadSchedules();
 });
--- server/routes/api.js
+++ server/routes/api.js
@@ -17,6 +17,67 @@ export function apiRoutes(app) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 
+    const schedulesOptionsFilePath = path.resolve(__dirname, '../data', 'schedulesOptions.txt');
+    const getSchedulesOptions = () => fs.readFileSync(schedulesOptionsFilePath, 'utf-8').toString().split('\n').map(l => l.trim());
+    const schedulesFilePath = path.resolve(__dirname, '../data', 'schedules.txt');
+    const getSchedulesHHMM = () => fs.readFileSync(schedulesFilePath, 'utf-8').toString().split('\n').map(l => l.trim());
+
+    const isMissing = (field) => field === undefined || field === '';
+    
+
+    app.route('/api/schedules')
+        .get((req, res) => {
+            try {
+                const schedulesHHMM = getSchedulesHHMM();
+                res.json({ schedules: schedulesHHMM });
+            } catch (error) {
+                console.error(error);
+            }
+
+            return;
+        })
+        .post((req, res) => {
+            try {
+                // console.log(req.body);
+                const { schedule, index } = req.body;
+                if (isMissing(index) || !Number.isInteger(index)) {
+                    res.json({ message: 'schedules index error' });
+                    return;
+                }
+                let schedulesHHMM = getSchedulesHHMM();                
+                if (index < 0 || index >= schedulesHHMM.length) {
+                    res.json({ message: 'schedules index error' });
+                    return;
+                }
+                if (isMissing(schedule)) {
+                    res.json({ message: 'schedules format error' });
+                    return;
+                }
+                const schedulesOptions = getSchedulesOptions();
+                if (!schedulesOptions.includes(schedule)) {
+                    res.json({ message: 'schedules not found' });
+                    return;
+                }
+                schedulesHHMM[index] = schedule;
+                fs.writeFileSync(schedulesFilePath, schedulesHHMM.join('\r\n'));
+                res.json({ schedule: schedule, index: index });
+            } catch (error) {
+                console.error(error);
+            }
+
+            return;
+        });
+
+    app.route('/api/schedules-options').get((req, res) => {
+        try {
+            const schedulesOptions = getSchedulesOptions();
+            res.json({ schedulesOptions: schedulesOptions });
+        } catch (error) {
+            console.error(error);
+        }
+
+        return;
+    });
 
     app.route('/api/webcam').get((req, res) => {
         try {
