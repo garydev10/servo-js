7a601e418636569131775d0001e4fee4be28d305
GaryDev <112358055+garydev10@users.noreply.github.com>
2025-04-27 15:13:23 +0100

    add boiler on off state to image filename

--- server/services/webcam.js
+++ server/services/webcam.js
@@ -3,6 +3,7 @@ import { writeFile } from 'node:fs';
 import NodeWebcam from 'node-webcam';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { getAverageColor } from 'fast-average-color-node';
 
 import { getDaylightTimeString } from './common.js';
 
@@ -22,16 +23,34 @@ const getWebCamImage = (callback) => {
 
     NodeWebcam.capture('temp-image', opts, function (err, data) {
 
-        const ds = getDaylightTimeString().replace('Z', '').replace(/[^0-9]/g, '-').substring(2, 19);
-
-        const name = `boiler-${ds}.${opts.output}`;
-        const fullPath = resolve(__dirname, '../public/images', name);
-
-        const base64Data = data.replace(/^data:image\/jpeg;base64,/, '');
-        writeFile(fullPath, base64Data, 'base64', function (err) {
-            if (err) {
-                throw err;
+        const datetimeString = getDaylightTimeString().replace('Z', '').replace(/[^0-9]/g, '-').substring(2, 19);
+        getAverageColor(data, {
+            left: 110,
+            top: 330,
+            width: 60,
+            height: 60
+        }).then(color => {
+            let onOff = '';
+            if (color) {
+                const red = color.value[0];
+                if (red >= 127) {
+                    onOff = '-on';
+                } else {
+                    onOff = '-off';
+                }
             }
+            const fileName = `boiler-${datetimeString}${onOff}.${opts.output}`;
+            const fullPath = resolve(__dirname, '../public/images', fileName);
+
+            const base64Data = data.replace(/^data:image\/jpeg;base64,/, '');
+            writeFile(fullPath, base64Data, 'base64', function (err) {
+                if (err) {
+                    throw err;
+                }
+            });
+        }).catch(error => {
+            console.error(error);
+            throw error;
         });
 
         const image = `<img src='${data}'>`;
