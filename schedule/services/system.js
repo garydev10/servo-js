d14af40f1e456db92e8e5c3321fee3660eb86c62
GaryDev <112358055+garydev10@users.noreply.github.com>
2025-05-21 23:49:54 +0100

    fix non-stop ping in schedule

--- schedule/services/system.js
+++ schedule/services/system.js
@@ -4,9 +4,9 @@ import { promisify } from "node:util";
 import { exec } from "node:child_process";
 
 const execAsync = promisify(exec);
+const platform = process.platform;
 
 const rebootSystem = async () => {
-  const platform = process.platform;
   if (platform !== "linux") return;
   const { stdout, stderr } = await execAsync("reboot");
   console.log(`stdout: ${stdout}`);
@@ -16,8 +16,12 @@ const rebootSystem = async () => {
 const getNetworkStatus = async () => {
   let isDown = false;
   const gatewayIp = "192.168.1.1";
+  let cmd = `ping ${gatewayIp}`;
+  if (platform === "linux") {
+    cmd = `${cmd} -c 3`;
+  }
   try {
-    await execAsync(`ping ${gatewayIp}`, { timeout: 5000 });
+    await execAsync(cmd, { timeout: 5000 });
   } catch (error) {
     console.error(error);
     isDown = true;
