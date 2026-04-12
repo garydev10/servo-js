a7694a818e7d9cee17ed4c526df094a3f56c56ea
GaryDev <112358055+garydev10@users.noreply.github.com>
2026-04-12 14:34:41 +0100

    add frame to webcam capture

--- server/services/webcam.js
+++ server/services/webcam.js
@@ -1,6 +1,7 @@
 "use strict";
 import { writeFile } from "node:fs";
 import NodeWebcam from "node-webcam";
+import sharp from 'sharp';
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { getAverageColor } from "fast-average-color-node";
@@ -25,12 +26,22 @@ const getWebCamImage = (callback) => {
       .replace("Z", "")
       .replace(/[^0-9]/g, "-")
       .substring(2, 19);
-    getAverageColor(data, {
+
+    const onOffLightRect = {
       left: 110,
       top: 330,
       width: 60,
       height: 60,
-    })
+    };
+    // Create an SVG rectangle as the frame
+    const svgRect = `
+      <svg width="${opts.width}" height="${opts.height}">
+      <rect x="${onOffLightRect.left}" y="${onOffLightRect.top}" width="${onOffLightRect.width}" height="${onOffLightRect.height}"
+        fill="none" stroke="green" stroke-width="4" />
+      </svg>
+    `;
+
+    getAverageColor(data, onOffLightRect)
       .then((color) => {
         let onOff = "";
         if (color) {
@@ -44,21 +55,31 @@ const getWebCamImage = (callback) => {
         const fileName = `boiler-${datetimeString}${onOff}.${opts.output}`;
         const fullPath = resolve(__dirname, "../public/images", fileName);
 
+        // Overay the rectangle on the image data
         const base64Data = data.replace(/^data:image\/jpeg;base64,/, "");
-        writeFile(fullPath, base64Data, "base64", function (err) {
-          if (err) {
-            throw err;
-          }
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        throw error;
+        const dataBuffer = Buffer.from(base64Data, "base64");
+        sharp(dataBuffer)
+          .composite([{ input: Buffer.from(svgRect), top: 0, left: 0 }])
+          .toBuffer()
+          .then((outputBuffer) => {
+
+            const data2 = `data:image/jpeg;base64,${outputBuffer.toString("base64")}`;
+            const image = `<img src='${data2}'>`;
+            callback(image);
+            
+            writeFile(fullPath, outputBuffer, function (err) {
+              if (err) {
+                throw err;
+              }
+            });
+
+          })
+          .catch((error) => {
+            throw error;
+          });
+
       });
 
-    const image = `<img src='${data}'>`;
-
-    callback(image);
   });
 };
 
